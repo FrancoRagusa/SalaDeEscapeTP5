74f90680fb8f1384c2a90104ec0d5a09bdd8a6b9
MATÍAS NICOLÁS ESTEVES <47434207@ort>
2023-06-22 15:53:45 -0300

    subido

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,59 +13,52 @@ public class HomeController : Controller
         _logger = logger;
     }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
     public IActionResult InicializarJuego()
-{
-    return View();
-}
- public IActionResult Comenzar()
-{
-    int estadoJuego = Escape.GetEstadoJuego();
-    string Sala = "sala" + (estadoJuego+1);
-    return View(Sala);
-}
-
-public IActionResult Index()
-{
-    return View();
-}
-
-
-[HttpPost] public IActionResult Habitacion(string clave, int sala)
-{
-    Escape.InicializarJuego();
-    int estadoJuego = Escape.GetEstadoJuego();
-
-    bool correcto = Escape.ResolverSala(sala, clave);
     {
-        if (!correcto)
-        {
-            ViewBag.paso = "no";
-        }
-        else
-        {
-            ViewBag.paso="si";
-        }
-        } 
-        if (Escape.GetEstadoJuego() == 5)
-        {
-        return View("salafinal");
-        
-        }
-        return View("Habitacion " + Escape.GetEstadoJuego());
+        return View();
     }
 
-    // ViewBag.Error = "Respuesta incorrecta.";
-    // return View("Habitacion : " + estadoJuego);
 
 
-public IActionResult Tutorial()
-{
-    return View("");
-}
+    public IActionResult Index()
+    {
+        return View();
+    }
 
+
+    [HttpPost] public IActionResult Habitacion(string clave, int sala)
+    {
+
+        bool correcto = Escape.ResolverSala(sala, clave);
+        {
+            if (!correcto)
+            {
+                ViewBag.error = "Tu respuesta es Incorrecta";
+            }
+
+            if (Escape.GetEstadoJuego() == 5)
+            {
+                return View("salafinal");            
+            }
+            return View("sala" + Escape.GetEstadoJuego());
+        }
+    }
+
+    public IActionResult Tutorial()
+    {
+        return View();
+    }
+    public IActionResult Comenzar()
+    {
+        Escape.InicializarJuego();
+        int estadoJuego = Escape.GetEstadoJuego();
+        string Sala = "sala" + estadoJuego;
+        return View(Sala);
+    }
 }
 
--- Models/Escape.cs
+++ Models/Escape.cs
@@ -7,20 +7,22 @@ public static int vidas{get; private set;}
 
 public static void InicializarJuego()
 {
+    
     incognitasSalas = new string[]{"Arbol", "Lobo", "Pinocho", "Mosquito"};
     estadoJuego=1;
 }
 
 public static int GetEstadoJuego()
 {
+    
     return estadoJuego;
 }
 
 public static bool ResolverSala(int Sala, string Incognita)
     {
 
-    
-    if(incognitasSalas[Sala] == Incognita)
+
+    if(incognitasSalas[Sala-1] == Incognita)
         {
             estadoJuego=estadoJuego+1;
             return true;
